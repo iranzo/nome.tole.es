58c71daedbbe04a01992f3e81a2414bb90986d44
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-11-20 11:13:36 +0100

    Initial commit

---
+++ content/pages/about.md
@@ -0,0 +1,12 @@
+---
+title: Acerca de
+layout: post
+date: 2016-06-02 17:27:47 +0200
+comments: true
+tags:
+description:
+---
+
+### ¿Quienes somos?
+
+Somos un grupo de madres descubriendo 
---
+++ pelicanconf.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+import datetime
+
+AUTHOR = u'Madres desatadas'
+SITENAME = u"No me tole.es"
+SITESUBTITLE = u'Consejos para la vida'
+SITEURL = u'/'
+TWITTER_USERNAME = ""
+# https://github.com/alexandrevicenzi/Flex/wiki/Custom-Settings
+MAIN_MENU = False
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Madrid'
+
+# Put as draft content in the future
+WITH_FUTURE_DATES = False
+
+# Put full text in RSS feed
+RSS_FEED_SUMMARY_ONLY = False
+
+# Default theme language.
+I18N_TEMPLATES_LANG = 'es'
+
+DEFAULT_LANG = 'es'
+OG_LOCALE = 'es_ES'
+LOCALE = 'es_ES'
+
+DEFAULT_CATEGORY = 'vida'
+
+# Feed generation is usually not desired when developing
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss'
+TRANSLATION_FEED_ATOM = 'feeds/%s.atom.xml'
+TRANSLATION_FEED_RSS = 'feeds/%s.rss'
+AUTHOR_FEED_ATOM = 'feeds/%s.atom.xml'
+AUTHOR_FEED_RSS = 'feeds/%s.rss'
+TAG_FEED_ATOM = 'feeds/tag_%s.atom.xml'
+TAG_FEED_RSS = 'feeds/tag_%s.rss'
+
+DISPLAY_PAGES_ON_MENU = True
+
+STATIC_PATHS = [
+    'imagen',
+    'extra/robots.txt',
+    'extra/favicon.ico',
+    'extra/google3bc953001343abe6'
+]
+
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/google3bc953001343abe6' : {'path': 'google3bc953001343abe6.html'}
+}
+
+CACHE_CONTENT = False
+CACHE_PATH = '.cache'
+LOAD_CONTENT_CACHE = False
+
+# Plugins
+PLUGIN_PATHS = ['plugins']
+
+PLUGINS = [
+    'better_codeblock_line_numbering',
+    'better_figures_and_images',
+    'sitemap',
+    # 'yuicompressor',
+    'i18n_subsites'
+]
+
+# Enable i18n plugin, probably you already have some others here.
+# PLUGINS = ['i18n_subsites']
+# Enable Jinja2 i18n extension used to parse translations.
+JINJA_EXTENSIONS = ['jinja2.ext.i18n']
+
+FAVICON = 'extra/favicon.ico'
+THEME = 'themes/Flex'
+
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
+USE_FOLDER_AS_CATEGORY = False
+
+SEARCH_BOX = False
+
+AUTHOR_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+
+ARCHIVES_URL = 'blog/archives/'
+ARCHIVES_SAVE_AS = 'blog/archives/index.html'
+
+CATEGORIES_URL = 'blog/categories/'
+CATEGORIES_SAVE_AS = 'blog/categories/index.html'
+
+TAGS_URL = 'blog/tags/'
+TAGS_SAVE_AS = 'blog/tags/index.html'
+
+DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'archives']
+
+LINKS = ()
+
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 5
+DEFAULT_ORPHANS = 0
+
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# better codeblock
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight', 'linenums': False},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+    },
+    'output_format': 'html5',
+}
+
+# sitemap
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+SITE_UPDATED = datetime.date.today()
+
+GOOGLE_ANALYTICS = "UA-81705-15"
+
+
+# blue-penguin
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL = 'tags'
+TAGS_SAVE_AS = 'tags/index.html'
+AUTHORS_URL = 'authors'
+AUTHORS_SAVE_AS = 'authors/index.html'
+CATEGORIES_URL = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL = 'archives'
+ARCHIVES_SAVE_AS = 'archives/index.html'
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    ('Tags', TAGS_URL, TAGS_SAVE_AS),
+    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+)
+
+DISQUS_SITENAME = "nome-tole-es"
+DISQUS_DISPLAY_COUNTS = True
